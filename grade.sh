cb62d579d79f66a4dda1be7994ba150d826eb3fc
Pranav Reddy <pranav.n.reddy@gmail.com>
2022-02-09 18:22:34 -0800

    added stuff

--- grade.sh
+++ grade.sh
@@ -2,5 +2,11 @@
 git clone $1
 cp GradingTests.java markdown-parse/
 cd markdown-parse
+
+if [ -e MarkdownParse.java ]
+	then
+		echo "file exists"
+fi
+
 javac -cp lib/*:. GradingTests.java MarkdownParse.java
 java -cp lib/*:. org.junit.runner.JUnitCore GradingTests
